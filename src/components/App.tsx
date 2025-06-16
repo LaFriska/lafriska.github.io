1ad0622f72c47ca6e56cdbd0675f1db8361a41aa
Harold Gao <harold.gao205@gmail.com>
2025-06-16 19:04:37 +1000

    added react markdowns

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import '../App.css';
 import Header from "./Header";
+import ReactMarkdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
 
 function App() {
   return (
