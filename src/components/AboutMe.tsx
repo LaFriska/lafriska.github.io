6000bf947d3919e379c48e70b8005cad356eccaa
Harold Gao <harold.gao205@gmail.com>
2025-06-17 22:33:41 +1000

    added connect with me

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -2,6 +2,7 @@ import {useEffect, useState} from "react";
 import ReactMarkdown from "react-markdown";
 import '../css/App.css';
 import ImageGallery from "./ImageGallery";
+import SocialLink from "./SocialLink";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
@@ -14,11 +15,26 @@ export default () => {
             .then(setAboutMe)
     }, [])
 
+    const socialLinksStyle = {
+
+    }
+
     return (
         <div style={{ display: 'flex' }}>
-            <ReactMarkdown className={'page'}>
-                {aboutMe}
-            </ReactMarkdown>
+            <div className={'page'}>
+                <ReactMarkdown>
+                    {aboutMe}
+                </ReactMarkdown>
+                <ReactMarkdown>
+                    {'## Connect With Me'}
+                </ReactMarkdown>
+                <div style={socialLinksStyle}>
+                    <SocialLink logoFile={'github.png'} text={'LaFriska'} link={'https://github.com/LaFriska'} />
+                    <SocialLink logoFile={'linkedin.png'} text={'Harold Gao'} link={'https://www.linkedin.com/in/harold-gao-4549342b8/'} />
+                    <SocialLink logoFile={'researchgate.png'} text={'Harold Gao'} link={'https://www.researchgate.net/profile/Harold-Gao?ev=hdr_xprf'} />
+                    <SocialLink logoFile={'mail.png'} text={'harold.gao205@gmail.com'} link={'harold.gao205@gmail.com'} />
+                </div>
+            </div>
             <ImageGallery/>
         </div>
     )
