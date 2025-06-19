3b5f679f5d06446f6192844b402216a7fbd9258a
Harold Gao <harold.gao205@gmail.com>
2025-06-19 17:13:15 +1000

    added project footer

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -8,17 +8,27 @@ export default () => {
         borderRadius: '3px',
         padding: '10px 15px',
         margin: '1rem 0',
-
+        gap: '10px'
     }
 
     const textContainerStyle = {
         marginLeft: '15px'
     }
 
+    const footerStyle = {
+        marginLeft: '15px',
+        fontSize: '0.75rem',
+        color: 'var(--border-gray)'
+    }
+
     return (
         <div style={containerStyle}>
             <div style={textContainerStyle}>
-                <h3 className={'projectText'}>Hello</h3>
+                <h3 className={'projectText'}>Kompakt</h3>
+                <p>A compact and convenient JSON serialiser for Java, aimed to resolve verbosity of libraries such as Gson.</p>
+            </div>
+            <div style={footerStyle}>
+                01/12/2024
             </div>
         </div>
     )
