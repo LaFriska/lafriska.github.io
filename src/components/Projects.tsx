b318c738043d5ecb0c904c4e92bac2702a7be5b3
Harold Gao <harold.gao205@gmail.com>
2025-06-19 16:21:21 +1000

    updated projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,8 +1,24 @@
+import '../css/App.css';
 
 export default () => {
-    return (
-        <div>
 
+    const style = {
+        display: 'grid',
+        gridTemplateColumns: 'repeat(3, 1fr)',
+        gap: '1rem'
+
+    }
+
+    return (
+        <div className={'page'} style={style}>
+            <div>1</div>
+            <div>2</div>
+            <div>3</div>
+            <div>4</div>
+            <div>5</div>
+            <div>6</div>
+            <div>7</div>
+            <div>8</div>
         </div>
     )
 }
